33722e252fa80b59455584e3b9ed84df58fa4453
Nick Cohen <nick@nickcohen.com>
2011-03-26 14:15:49 -0700

    hide respond to post by default

--- post.php
+++ post.php
@@ -36,36 +36,27 @@
 	<title>Greenpride Mobile</title>
 	<?php echo head_links(); ?>
 	<script language="javascript">
-	function toggleParent() {
-		var text = ' Previous Message';
-		if($('#parent').css('display') == 'none') {
-			text = 'Hide' + text;
+	function toggleDiv(divid, buttonid, showtext, hidetext) {
+		//var text = ' Previous Message';
+		var text;
+		if($('#' + divid).css('display') == 'none') {
+			//text = 'Hide ' + text;
+			text = hidetext;
 		}
 		else {
-			text = 'Show' + text;
+			//text = 'Show ' + text;
+			text = showtext;
 		}
-		$('#parentToggle').val(text);
-		$('#parent').toggle();
-	}
-	function toggleThread() {
-		var text = ' Thread';
-		if($('#thread').css('display') == 'none') {
-			text = 'Hide' + text;
-		}
-		else {
-			text = 'Show' + text;
-		}
-		$('#threadToggle').val(text);
-		$('#thread').toggle();
-	}
-	
+		$('#' + buttonid).val(text);
+		$('#' + divid).toggle();
+	}	
 	</script
 </head>
 <body>
 <div class="panel" selected="true">
 
 
-<input id="threadToggle" type="button" class="whiteButton" value="Show Thread" onclick="javascript:toggleThread()" />
+<input id="threadToggle" type="button" class="whiteButton" value="Show Thread" onclick="javascript:toggleDiv('thread', 'threadToggle', 'Show Thread', 'Hide Thread')" />
 <div id="thread" style="display:none; margin-left: 0; padding-left: 0; border: 1px solid #999999;">
 <?php
 	$settings = array(
@@ -75,7 +66,7 @@
 ?>
 </div>
 <?php if(!empty($parent)) { ?>
-<input id="parentToggle" type="button" class="whiteButton" value="Show Previous Message" onclick="javascript:toggleParent()" />
+<input id="parentToggle" type="button" class="whiteButton" value="Show Previous Message" onclick="javascript:toggleDiv('parent', 'parentToggle', 'Show Previous Message', 'Hide Previous Message')" />
 <div id="parent" class="parent" style="display:none; border: 1px solid #999999;">
 <table>
 	<tr>
@@ -108,20 +99,36 @@
 		'search' => FALSE
 	);
 	echo menu($menuSettings);
-	
-	echo '<b>';
-	echo 'Author: '.$post['AuthorName'];
-	echo '<BR>';
-	echo 'Subject: ' .$post['Subject'];
-	echo '<BR>';
-	echo 'Date: ' .formatDate($post['Date']);
-	echo '<BR>';
-	echo preg_replace('/\n/', '<BR>', $post['Description']);
-	echo '<BR>';
-	echo '</b>';
-	
 ?>
+<style>
+.heading table,th,td,tr {
+	text-align: left;
+}
+</style>
+<div class="heading">
+<table>
+	<tr>
+		<th>Author</th>
+		<td><?php echo $post['AuthorName']; ?>
+	</tr>
+	<tr>
+		<th>Subject</th>
+		<td><?php echo $post['Subject']; ?>
+	</tr>
+	<tr>
+		<th>Date</th>
+		<td><?php echo formatDate($post['Date']); ?>
+	</tr>
+</table>
+<div class="post">
+<?php
+	echo preg_replace('/\n/', '<BR>', $post['Description']);
+?>
+</div>
+	
 
+<input id="responseToggle" type="button" class="whiteButton" value="Respond to Post" onclick="javascript:toggleDiv('response', 'responseToggle', 'Respond to Post', 'Hide Respond to Post')" />
+<div id="response" style="display:none; margin-left: 0; padding-left: 0; border: 1px solid #999999;">
 <form id="postsubmit" class="panel" action="/postsubmit.php" method="POST" > 
 	<fieldset>
 <?php
@@ -143,6 +150,7 @@
 	<input type="submit" class="whiteButton" value="Submit" />
 	<input type="hidden" name="postid" value="<?php echo $postid; ?>" />
 </form> 
+</div>
 
 <?php
 	$menuSettings = array(
