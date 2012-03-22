ed17ede9e453c1f50f23becc15a881f71594d516
Nick Cohen <nick@nickcohen.com>
2012-03-21 20:20:31 -0700

    links clickable

--- util.php
+++ util.php
@@ -305,11 +305,40 @@
 		echo "
 			</ul>";
 	}
+
+	function makeLinksClickable($text) {
+
+                #preg_replace('/https?:\/\/[^\s<]+/i', '<a href="\0">\0</a>', $text);
+
+                # this functions deserves credit to the fine folks at phpbb.com
+                $text = preg_replace('#(script|about|applet|activex|chrome):#is', "\\1:", $text);
+
+                // pad it with a space so we can match things at the start of the 1st line.
+                $ret = ' ' . $text;
+
+                // matches an "xxxx://yyyy" URL at the start of a line, or after a space.
+                // xxxx can only be alpha characters.
+                // yyyy is anything up to the first space, newline, comma, double quote or <
+                $ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret);
+
+                // matches a "www|ftp.xxxx.yyyy[/zzzz]" kinda lazy URL thing
+                // Must contain at least 2 dots. xxxx contains either alphanum, or "-"
+                // zzzz is optional.. will contain everything up to the first space, newline,
+                // comma, double quote or <.
+                $ret = preg_replace("#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $ret);
+
+                // matches an email@domain type address at the start of a line, or after a space.
+                // Note: Only the followed chars are valid; alphanums, "-", "_" and or ".".
+                $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
+
+                // Remove our padding..
+                $ret = substr($ret, 1);
+                return $ret;
+        }
 	
 	function pretty_pretty($text) {
 		$text = preg_replace('/\n/', '<BR>', $text);
-		//$text = preg_replace('/https?:\/\/[^\s<]+/i', '<a target="_blank" href="\0">\0</a>', $text);
-		//$text = preg_replace('/((?!href=).*)https?:\/\/[^\s<]+/i', '<a target="_blank" href="\0">\0</a>', $text);
+		$text = makeLinksClickable($text);
 		return $text;
 	}
 ?>
